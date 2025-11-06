8843091522f2c501a28159a2eca837a2ada85644
Karol <karolfassel@gmail.com>
2025-11-06 14:16:00 -0300

    criando classe principal Menu e classe Cores

---
+++ Menu.ts
@@ -0,0 +1,97 @@
+import readlinesync = require("readline-sync");
+import { colors } from "./src/util/Colors";
+
+export function main(){
+    let opcao: number;
+    while(true){
+        console.log(colors.bg.black, colors.fg.yellow,"****************************************************");
+        console.log("                                                    ");
+        console.log("              BANCO DO BRAZIL COM Z                 ");
+        console.log("                                                    ");
+        console.log("****************************************************");
+        console.log("                                                    ");
+        console.log("              1 - Criar Conta                       ");
+        console.log("              2 - Listar todas as Contas            ");
+        console.log("              3 - Buscar Conta por Numero           ");
+        console.log("              4 - Atualizar Dados da Conta          ");
+        console.log("              5 - Apagar Conta                      ");
+        console.log("              6 - Sacar                             ");
+        console.log("              7 - Depositar                         ");
+        console.log("              8 - Transferir valores entre Contas   ");
+        console.log("              9 - Sair                              ");
+        console.log("                                                    ");
+        console.log("****************************************************");
+        console.log("                                                    ",
+        colors.reset);
+       
+
+        console.log("Entre com a opção desejada: ");
+        opcao = readlinesync.questionInt("");
+
+        if(opcao ==9){
+            console.log(colors.fg.greenstrong,"\nBanco do Brazil com Z - O seu futuro começa aqui!");
+            sobre();
+        console.log(colors.reset,"");
+            process.exit(0);
+        }
+
+        switch(opcao){
+            case 1:
+                console.log(colors.fg.whitestrong,"\n\nCriar conta\n\n", colors.reset);
+                keyPress()
+                break;
+            case 2:
+                console.log(colors.fg.whitestrong,"\n\nListar todas as contas\n\n", colors.reset);
+                keyPress()
+                break;
+            case 3:
+                 console.log(colors.fg.whitestrong,"\n\nConsultar dados da conta - por numero\n\n", colors.reset);  
+                 keyPress()
+                 break;
+            case 4:
+                 console.log(colors.fg.whitestrong,"\n\nAtualizar dados da conta\n\n", colors.reset);  
+                 keyPress()
+                 break;
+            case 5:
+                 console.log(colors.fg.whitestrong,"\n\nApagar uma conta\n\n", colors.reset); 
+                 keyPress()
+                 break;
+            case 6:
+                 console.log(colors.fg.whitestrong,"\n\nSaque\n\n", colors.reset);  
+                 keyPress()
+                 break;
+            case 7:
+                 console.log(colors.fg.whitestrong,"\n\nDeposito\n\n", colors.reset);   
+                 keyPress()
+                 break;
+            case 8:
+                 console.log("\n\nTranferencia entre contas\n\n", colors.reset);    
+                 keyPress()
+                 break;
+            default:
+                 console.log(colors.fg.whitestrong,"\n\nOpcao invalida!\n\n", colors.reset);  
+                
+                 keyPress()
+                 break;
+        }
+
+    }
+}
+
+export function sobre(): void{
+     console.log("\n****************************************************");
+     console.log("Projeto Desenvolvido por: ");
+     console.log("Karoline - karolfassel@generation.org");
+     console.log("github.com/karolsfassel");
+     console.log("\n****************************************************");
+}
+
+main();
+
+
+function keyPress():void{
+    console.log(colors.reset, "");
+    console.log("\nPressione enter para continuar...");
+    readlinesync.prompt();
+    
+}
